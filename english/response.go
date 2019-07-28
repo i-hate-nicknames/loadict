7a2a419fdd4ac61a28f1a68e65130ae5aed67d62
Never M <nevermn@protonmail.com>
2019-07-28 18:54:11 +0300

    Add english dictionary response structure

---
+++ english/response.go
@@ -0,0 +1,40 @@
+package english
+
+type response struct {
+    results []*Result
+}
+
+type Result struct {
+    LexicalEntries []*LexicalEntry
+    Word string
+}
+
+type LexicalEntry struct {
+    LexicalCategory string
+    Entries []*Entry
+    Pronunciations []*Pronunciation
+}
+
+type Entry struct {
+    GrammaticalFeatures []*GrammaticalFeature
+    Senses []*Sense
+}
+
+type GrammaticalFeature struct {
+    Text string
+    Type string
+}
+
+type Sense struct {
+    Definitions []string
+    Examples []*Example
+}
+
+type Example struct {
+    Text string
+}
+
+type Pronunciation struct {
+    PhoneticSpelling string
+    Dialects []string
+}
