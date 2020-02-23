aa1c6ccf110eaa05bcc48261fd47c0c63939ab0a
Never M <nevermn@protonmail.com>
2020-02-23 21:58:43 +0200

    Use godoenv to load .env config

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module nvm.ga/loadict
 
 go 1.13
+
+require github.com/joho/godotenv v1.3.0
--- loadict.go
+++ loadict.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	appId := os.Getenv("APP_ID")
+	fmt.Println("App id: " + appId)
 	fmt.Println("Words loader for anki will be here some day")
 }
