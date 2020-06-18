f771aea36ade2852fc208355f514541f6717ddc8
nvm <nevermn@protonmail.com>
2020-06-18 19:50:28 +0300

    Add todos for future improvements

--- main.go
+++ main.go
@@ -16,6 +16,16 @@ var load = flag.Bool("load", false, "loads list of words and stores them locally
 var export = flag.Bool("export", false, "exports a number of card into csv file")
 var exportNumber = flag.Int("n", exportCardsNum, "number of cards to export, default 10")
 
+// use n for loading too, to limit the number of cards loaded from the api
+// handle 403 errors in case we are making too many requests somehow
+// change load to "prefetch", change load flag so that it simply adds words
+// to the local db as pending words
+// change export feature so that it if there are no ready to export cards it will try to
+// prefetch pending words
+// effectively, you would rarely want to run "prefetch" on its own, and prefer adding words
+// in pending state and let export to run "prefetch" for you
+// but, in case you want to prefetch your words
+
 func main() {
 	flag.Parse()
 	if !*load && !*export {
